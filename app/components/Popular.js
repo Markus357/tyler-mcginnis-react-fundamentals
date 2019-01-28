f708e86a38aa2a23adfba59deefd2f142bf225c6
Mark Davidson <mark.davidson@Comp-191.local>
2019-01-28 16:58:52 -0500

    Refactor Popular component to manage state and pass data down to stateless functional components SelectLanguage (menu) and RepoGrid

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,34 +1,94 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import api from '../utils/api';
+
+function SelectLanguage( props ) {
+  var languages = [ 'All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python' ];
+
+  return (
+    <ul className="languages">
+      { languages.map( lang => (
+        <li
+          className={ lang === props.selectedLanguage ? 'selected' : '' }
+          onClick={ props.onSelect.bind( null, lang ) }
+          key={ lang }>
+
+          { lang }
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired,
+};
+
+function RepoGrid ( props ) {
+  return (
+    <ul className="popular-list">
+      { props.repos.map(( repo, index ) => (
+        <li key={ repo.name } className="popular-item">
+          <div className="popular-rank"># { index+1 }</div>
+          <ul className="space-list-items">
+            <li>
+              <img
+                className="avatar"
+                src={ repo.owner.avatar_url }
+                alt={ 'Avatar for ' + repo.owner.login }
+              />
+            </li>
+            <li><a href={ repo.html_url }>{ repo.name }</a></li>
+            <li>@{ repo.owner.login }</li>
+            <li>{ repo.stargazers_count } stars</li>
+          </ul>
+        </li>
+      ) ) }
+    </ul>
+  )
+}
+
+RepoGrid.propTypes = {
+  repos: PropTypes.array.isRequired,
+};
 
 class Popular extends React.Component {
   constructor ( props ) {
     super( props );
 
-    this.updateLanguage = this.updateLanguage.bind( this );
-
     this.state = {
       selectedLanguage: 'All',
+      repos: null,
     };
   }
 
   updateLanguage = lang => {
-    return this.setState( prev => ({ selectedLanguage: lang }));
+    this.setState( prev => ({ selectedLanguage: lang, repos: null }));
+
+    api
+      .fetchPopularRepos( lang )
+      .then( repos => this.setState( _ => ({ repos }) ) );
+  }
+
+  componentDidMount() {
+    this.updateLanguage( this.state.selectedLanguage );
   }
 
   render() {
-    var languages = [ 'All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python' ];
-
     return (
-      <ul className="languages">
-        { languages.map( lang => (
-          <li
-            className={ lang === this.state.selectedLanguage ? 'selected' : '' }
-            onClick={ this.updateLanguage.bind( null, lang ) }
-            key={ lang }>
-            { lang }
-          </li>
-        ))}
-      </ul>
+      <div>
+        <SelectLanguage
+          selectedLanguage={ this.state.selectedLanguage }
+          onSelect={ this.updateLanguage }
+        />
+
+        { !this.state.repos
+          ? <h2 style={{ textAlign: 'center' }}>Loading...</h2>
+          : <RepoGrid repos={ this.state.repos } />
+        }
+      </div>
     );
   }
 }
