36e2e70022684db996a90a1fb57b55542f61cea8
Mark Davidson <mark.davidson@Comp-191.local>
2019-01-29 12:56:13 -0500

    Wire-up Router in main app file and pass relevant components

--- app/components/App.js
+++ app/components/App.js
@@ -1,14 +1,30 @@
-var React = require('react');
-
+import React from 'react';
+import Nav from './Nav';
+import Home from './Home'
 import Popular from './Popular'
+import Battle from './Battle'
+
+const ReactRouter = require('react-router-dom');
+const Router = ReactRouter.BrowserRouter;
+const Route = ReactRouter.Route;
+const Switch = ReactRouter.Switch;
 
 class App extends React.Component {
   render() {
     return (
-      <div className='container'>
-        <Popular />
-      </div>
-    )
+      <Router>
+        <div className='container'>
+          <Nav />
+
+          <Switch>
+            <Route exact path='/' component={ Home } />
+            <Route path='/battle' component={ Battle } />
+            <Route path='/popular' component={ Popular } />
+            <Route render={ () => <p>Not Found</p> } />
+          </Switch>
+        </div>
+      </Router>
+    );
   }
 }
 
